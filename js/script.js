475ae75a537d7d596775b0bb65916f98ae401f30
TITI3048 <thibaud_rouviere@yahoo.fr>
2024-09-26 10:53:15 +0200

    modif

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var button = document.querySelector('.button');
+        var menu = document.querySelector('.menu');
+
+        button.onclick = function() {
+            button.classList.toggle('active');
+            menu.classList.toggle('active');
+}
