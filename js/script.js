4b466ee8e2b35bc8c71ef4b157e6fd6a10951578
TITI3048 <thibaud_rouviere@yahoo.fr>
2024-09-26 13:53:14 +0200

    modif

--- js/script.js
+++ js/script.js
@@ -5,3 +5,18 @@ var button = document.querySelector('.button');
             button.classList.toggle('active');
             menu.classList.toggle('active');
 }
+
+window.onscroll = function() {
+    const button = document.getElementById("scrollToTop");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        button.style.display = "block"; // Affiche le bouton
+    } else {
+        button.style.display = "none"; // Cache le bouton
+    }
+};
+
+// Faire défiler la page vers le haut
+document.getElementById("scrollToTop").onclick = function() {
+    document.body.scrollTop = 0; // Pour Chrome, Safari et Opera
+    document.documentElement.scrollTop = 0; // Pour Internet Explorer et Firefox
+};
